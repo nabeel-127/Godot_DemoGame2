bbf72de59b74c0a003295ce86af232da82677c36
Nabeel Ali <nabeel1207ali@gmail.com>
2023-07-18 22:22:14 +0500

    migrated project from TestGame1

---
+++ Main.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://Settings.tscn")
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://TestLevel.tscn")
