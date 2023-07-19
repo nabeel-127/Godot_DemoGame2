c1c41b72b4495a345cb036cef6e4064a5a18ab62
Nabeel Ali <nabeel1207ali@gmail.com>
2023-07-19 06:44:07 +0500

    player movement (WIP)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,23 +1,50 @@
 extends CharacterBody2D
 
-const SPEED = 200
+const max_speed = 100
+const acceleration = 500
+const friction = 1000
+var input = Vector2.ZERO
 
 func _physics_process(delta):
-	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 10
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 10
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 10
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 10
-		
-#	if Input.action_release("ui_right"):
-#		velocity.x = 0
-	
-	velocity = velocity.normalized() * SPEED
+	PlayerMovement(delta)
 	move_and_slide()
+	
+# 	if Input.is_action_pressed("ui_right"):
+# 		velocity.x += 10
+# 	if Input.is_action_pressed("ui_left"):
+# 		velocity.x -= 10
+# 	if Input.is_action_pressed("ui_down"):
+# 		velocity.y += 10
+# 	if Input.is_action_pressed("ui_up"):
+# 		velocity.y -= 10
+		
+# #	if Input.action_release("ui_right"):
+# #		velocity.x = 0
+	
+# 	velocity = velocity.normalized() * SPEED
+# 	move_and_slide()
+func GetInput():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+
+func PlayerMovement(delta):
+	input = GetInput()
+
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += input * acceleration * delta
+		velocity = velocity.limit_length(max_speed)
+	
+
+
+
+
+
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
