d93ac01e50a851a63b284ea15a6a55f2241ce5db
Nabeel Ali <nabeel1207ali@gmail.com>
2023-08-12 04:13:51 +0500

    complete reorganization

--- Main.gd
+++ Main.gd
@@ -4,7 +4,7 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://Menus/Settings.tscn")
+	get_tree().change_scene_to_file("res://UI/Settings.tscn")
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/TestLevel.tscn")
