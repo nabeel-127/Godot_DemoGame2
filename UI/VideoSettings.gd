4fd9bd1c0dec6f80032da44269c299c4319551cd
Nabeel Ali <nabeel1207ali@gmail.com>
2023-09-03 20:14:41 +0500

    removed .import files

--- UI/VideoSettings.gd
+++ UI/VideoSettings.gd
@@ -1,14 +1,14 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
 
 func _on_back_button_pressed():
@@ -17,7 +17,6 @@ func _on_back_button_pressed():
 
 func _on_check_button_toggled(button_pressed):
 	if button_pressed:
-		print(button_pressed)		
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN, 3)
+		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, 0)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
