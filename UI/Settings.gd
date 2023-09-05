751da387d3d8940ee11df047a5c2bd548e2ee7a0
Nabeel Ali <nabeel1207ali@gmail.com>
2023-09-05 17:06:21 +0500

    ui improved

--- UI/Settings.gd
+++ UI/Settings.gd
@@ -2,7 +2,10 @@ extends Control
 
 
 
-
-
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Main.tscn")
+
+
+
+func _on_video_button_pressed():
+	get_tree().change_scene_to_file("res://UI/VideoSettings.tscn")
--- UI/VideoSettings.gd
+++ UI/VideoSettings.gd
@@ -1,21 +1,36 @@
 extends Control
 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+@onready var Resolution = $LeftPanel/HBoxContainer2/Resolution
+
+
+
+func _ready():
+	AddResolutions()
+
+
+
+func AddResolutions():
+	Resolution.remove_item(0)
+	Resolution.add_item("640x480")
+	Resolution.add_item("800x600")
+	Resolution.add_item("1024x768")
+	Resolution.add_item("1152x864")
+	Resolution.add_item("1280x800")
+	Resolution.add_item("1280x960")
+	Resolution.add_item("1440x1080")
+	Resolution.add_item("1920x1080")
+	Resolution.add_item("1920x1200")
+
 
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://UI/Settings.tscn")
 
 
-func _on_check_button_toggled(button_pressed):
+
+func _on_fullscreen_toggled(button_pressed):
 	if button_pressed:
 		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 	else:
